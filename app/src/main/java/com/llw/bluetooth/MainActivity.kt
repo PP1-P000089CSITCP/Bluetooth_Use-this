db6301dffa83e5bb9858054f7c752b8b7802fccf
Xinyu Chen <s3798356@student.rmit.edu.au>
2022-05-03 16:43:57 +0800

    main page

--- app/src/main/java/com/llw/bluetooth/MainActivity.kt
+++ app/src/main/java/com/llw/bluetooth/MainActivity.kt
@@ -6,6 +6,8 @@ import android.bluetooth.BluetoothDevice
 import android.content.*
 import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -248,5 +250,24 @@ class MainActivity : AppCompatActivity() {
             showMsg("你的设备不支持蓝牙")
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+
 }
 
