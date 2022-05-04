38e12c37705ed68f84206df08002cbb02abb043b
Duanyinglin <s3779079@student.rmit.edu.au>
2022-05-04 15:39:04 +0800

    Adding more battery_Information
    Adding Battery charging status, Power Source, Power Voltage

--- app/src/main/java/com/llw/bluetooth/BatteryInforamtion.kt
+++ app/src/main/java/com/llw/bluetooth/BatteryInforamtion.kt
@@ -24,7 +24,7 @@ class BatteryInforamtion : AppCompatActivity() {
 
             val batteryPercentage = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
 
-            stringBuilder.append("Battery percentage:\n", batteryPercentage)
+            stringBuilder.append("Battery percentage:\n", batteryPercentage,"%")
 
             stringBuilder.append("\n")
 
@@ -48,12 +48,48 @@ class BatteryInforamtion : AppCompatActivity() {
             val temperatureInCelsius = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10
 
 
-            stringBuilder.append(temperatureInCelsius)
+            stringBuilder.append(temperatureInCelsius,"°C\n")
+
+            val temperatureFahrenheit = ((temperatureInCelsius*1.8)+32).toInt()
+
+            stringBuilder.append(temperatureFahrenheit,"°F\n")
+
+            stringBuilder.append("\nPower source:\n")
+
+            when(intent.getIntExtra(BatteryManager.EXTRA_PLUGGED,-1)){
+                BatteryManager.BATTERY_PLUGGED_AC -> stringBuilder.append("AC adpter\n")
+                BatteryManager.BATTERY_PLUGGED_USB -> stringBuilder.append("USB connection\n")
+                BatteryManager.BATTERY_PLUGGED_WIRELESS -> stringBuilder.append("Wireless connection\n")
+                else ->
+                    stringBuilder.append("No Power Source\n")
+            }
+
+            stringBuilder.append("\nChargin status:\n")
+
+            when(intent.getIntExtra(BatteryManager.EXTRA_STATUS,-1)){
+                BatteryManager.BATTERY_STATUS_CHARGING -> stringBuilder.append("Charging\n")
+                BatteryManager.BATTERY_STATUS_DISCHARGING -> stringBuilder.append("Not Charging\n")
+                BatteryManager.BATTERY_STATUS_FULL -> stringBuilder.append("Full\n")
+                BatteryManager.BATTERY_STATUS_NOT_CHARGING -> stringBuilder.append("Not Charging\n")
+                BatteryManager.BATTERY_STATUS_UNKNOWN -> stringBuilder.append("Unknown\n")
+                else ->
+                    stringBuilder.append("Unknown\n")
+            }
+
+            val voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE,0).toDouble()/100
+
+            stringBuilder.append("\nVoltage:\n",voltage, "V\n")
+
 
             val myTextView = findViewById<TextView>(R.id.battery_information)
             myTextView.text = stringBuilder
 
 
         }
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(myBroadcastRecevier)
+        super.onDestroy()
     }
 }
