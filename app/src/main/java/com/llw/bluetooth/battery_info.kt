e30e9e01f82ca491614c25724fb1d7d380b695aa
SEFStudent <Penguinsarecute1996!>
2022-05-17 19:06:55 +1000

    battery_info.kt added to batter_tiles

---
+++ app/src/main/java/com/llw/bluetooth/battery_info.kt
@@ -0,0 +1,11 @@
+package com.llw.bluetooth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class battery_info : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_battery_info)
+    }
+}
